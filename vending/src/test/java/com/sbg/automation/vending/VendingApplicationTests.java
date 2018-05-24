fe9169ab78e90b146e911daebfe9575033656bf9
robin murphy <robin.murphy@standardbank.co.za>
2018-05-24 17:43:54 +0200

    added travis

--- vending/src/test/java/com/sbg/automation/vending/VendingApplicationTests.java
+++ vending/src/test/java/com/sbg/automation/vending/VendingApplicationTests.java
@@ -11,6 +11,7 @@ public class VendingApplicationTests {
 
 //	@Test
 	public void contextLoads() {
+
 	}
 
 }
--- vending/src/test/java/com/sbg/automation/vending/payment/PaymentProcessorTest.java
+++ vending/src/test/java/com/sbg/automation/vending/payment/PaymentProcessorTest.java
@@ -72,7 +72,7 @@ public class PaymentProcessorTest {
 
         Product product = new Product();
         product.setDescription("test1");
-        product.setItemValue(new Double(0));
+        product.setItemValue(new Double(10));
         product.setProductKey("test1");
         product.setQuantity(0);
         productList.add(product);
