10ae89d6758552b8fa473cd5124d5960e7985b5e
robin murphy <robin.murphy@standardbank.co.za>
2018-05-26 20:57:04 +0200

    added db

--- src/main/java/com/sbg/automation/vending/spring/ServiceConfiguration.java
+++ src/main/java/com/sbg/automation/vending/spring/ServiceConfiguration.java
@@ -1,10 +1,13 @@
 package com.sbg.automation.vending.spring;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.dozer.DozerBeanMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,5 +23,14 @@ public class ServiceConfiguration {
         DozerBeanMapper mapper = new DozerBeanMapper(mappingFiles);
         return mapper;
     }
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
 
 }
--- src/main/java/com/sbg/automation/vending/utils/CollectionMapperImpl.java
+++ src/main/java/com/sbg/automation/vending/utils/CollectionMapperImpl.java
@@ -3,10 +3,12 @@ package com.sbg.automation.vending.utils;
 import org.dozer.DozerBeanMapper;
 import org.dozer.MappingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CollectionMapperImpl implements CollectionMapper {
 
     @Autowired
